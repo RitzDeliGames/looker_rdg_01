2455a61ef7ce157a1bab66a15908621d7dd0e8d6
Vladyslav Zhylka <zhylkavladyslav@gmail.com>
2022-10-20 10:36:57 +0000

    log data

--- visualizations/2d_boxplot.js
+++ visualizations/2d_boxplot.js
@@ -6,6 +6,9 @@ looker.plugins.visualizations.add({
   element.innerHTML = "<h1>demo</h1>";
   },
   update: function(data, element, config, queryResponse){
-
+   console.log('data', data)
+   console.log('config', config)
+   console.log('queryResponse', queryResponse)
+   element.innerHTML = JSON.stringify(data)
   }
 })

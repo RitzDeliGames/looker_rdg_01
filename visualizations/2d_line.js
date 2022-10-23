267731ccf47b90aabcb40a7e4cdfabb58ecd1acb
Vladyslav Zhylka <zhylkavladyslav@gmail.com>
2022-10-23 07:30:56 +0000

    wip: 2d line visulaization

--- visualizations/2d_line.js
+++ visualizations/2d_line.js
@@ -3,12 +3,50 @@ looker.plugins.visualizations.add({
   label: "2D Line",
   options: {},
   create: function (element, config) {
-    element.innerHTML = "<h1>line chart</h1>";
+    element.innerHTML = "";
   },
   update: function (data, element, config, queryResponse) {
-    console.log("data", data);
-    console.log("config", config);
-    console.log("queryResponse", queryResponse);
-    element.innerHTML = JSON.stringify(data);
+    // console.log("data", data);
+    // console.log("config", config);
+    // console.log("queryResponse", queryResponse);
+    // element.innerHTML = JSON.stringify(data);
+    let series = [];
+  let x_dim_1 = queryResponse.fields.dimensions[1];
+  let x_dim_2 = queryResponse.fields.dimensions[0];
+  let y_dim = queryResponse.fields.table_calculations[3];
+
+  let minMeasureName = queryResponse.fields.measure_like[0]?.name;
+  let q25MeasureName = queryResponse.fields.measure_like[1]?.name;
+  let medMeasureName = queryResponse.fields.measure_like[2]?.name;
+  let q75MeasureName = queryResponse.fields.measure_like[3]?.name;
+  let maxMeasureName = queryResponse.fields.measure_like[4]?.name;
+
+  series.push({
+    name: "Last Level Completed",
+    data: data.map((row) => row[x_dim_1.name].value),
+  });
+
+  series.push({
+    name: "Experiment Variant",
+    data: data.map((row) => row[x_dim_2.name].value),
+  });
+
+
+  const options = {
+    legend: {
+      enabled: false,
+    },
+
+    yAxis: {
+      title: {
+        text: 'Churn per Minute',
+      },
+    },
+
+    series,
+  };
+
+  Highcharts.chart("container", options);
+
   },
 });

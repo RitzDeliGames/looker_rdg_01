86c09c5033615258879c9048cc28a934c7052700
Luis Vivas <luis@ritzdeligames.com>
2022-10-31 22:36:07 +0000

    fixing the labels

--- visualizations/2d_line.js
+++ visualizations/2d_line.js
@@ -189,7 +189,7 @@ looker.plugins.visualizations.add({
 
        //set an invalid display type so only the label renders
        option[id + "_label"] = {
-         label: id.toUpperCase() + " ----------------------------------------------------------------------------",
+         label: id.toUpperCase(),
          type: "string",
          display: "label",
          section: "Series",
